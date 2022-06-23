cb46549ec69e7f4ac2d26411a0f9853d989eef19
Brian Kim <brian@briankim.io>
2022-06-23 20:13:10 +0000

    Sync LeetCode submission - Two Sum (python3)

---
+++ two_sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums = list(enumerate(nums))  # O(n)
+        nums = sorted(nums, key=lambda x: x[1])  # O(n * log n)
+        nums.reverse()  # O(n)
+        
+        i = 0
+        j = len(nums) - 1
+        
+        solution = []
+        
+        # Rule out numbers that are too large
+        while (nums[i][1] + nums[j][1]) > target:
+            i += 1
+
+        while not solution:
+            while (nums[i][1] + nums[j][1]) <= target:
+                if nums[i][1] + nums[j][1] == target:
+                    solution += [nums[i][0], nums[j][0]]
+                    break
+                    
+                j -= 1
+                
+            i += 1
+                
+        return solution
+        
