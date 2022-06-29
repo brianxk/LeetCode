82d19c7c7e19113ebda0639000e5eb75b9674d99
Brian Kim <brian@briankim.io>
2022-06-29 18:05:17 +0000

    Sync LeetCode submission - Two Sum (python3)

--- two_sum.py
+++ two_sum.py
@@ -7,21 +7,17 @@ class Solution:
         i = len(nums) - 1
         j = 0
         
-        solution = []
-        
         # Rule out numbers that are too large
         while (nums[i][1] + nums[j][1]) > target:
             i -= 1
 
-        while not solution:
+        while i >= 0:
             while (nums[i][1] + nums[j][1]) <= target:
                 if nums[i][1] + nums[j][1] == target:
-                    solution += [nums[i][0], nums[j][0]]
-                    break
+                    return [nums[i][0], nums[j][0]]
                     
                 j += 1
                 
             i -= 1
                 
-        return solution
         
